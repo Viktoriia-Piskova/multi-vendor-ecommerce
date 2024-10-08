46092fae6169c644a665480c50c68c794beeb71a
piskova <v.piskova@beetroot.se>
2024-10-08 14:03:00 +0300

    Login and Register pages design

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+const App = lazy(() => import("./App"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+  <BrowserRouter>
+    <Suspense>
       <App />
-    </BrowserRouter>
+    </Suspense>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login page</h1>
+  return (
+    <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
+      <div className="w-[350px] text-white- p-2">
+        <div className="bg-[#6f68d1] p-4 rounded-md text-white">
+          <h2 className="text-xl mb-3 font-bold">Welcome to Ecommerce</h2>
+          <p className="text-sm mb-3 font-medium">Please log in</p>
+          <form>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="email">Email</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
+                type="email"
+                name="email"
+                placeholder="Email"
+                id="email"
+                required
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
+                type="password"
+                name="password"
+                placeholder="Password"
+                id="password"
+                required
+              />
+            </div>
+            <button className="bg-slate-800 w-full hover:shadow-blue-300/50 hover:shadow-md text-white rounded-md px-7 py-2 mb-3 transition-all duration-300 ease-in-out">
+              Log in
+            </button>
+            <div className="flex items-center mb-3 gap-3 justify-center">
+              <p>
+                Don't have an account?
+                <Link to="/register" className="font-bold ml-2 hover:underline">
+                   Register
+                </Link>
+              </p>
+            </div>
+            <div className="w-full flex justify-center items-center mb-3">
+              <hr className="w-[45%] bg-slate-700 h-[1px] border-none"></hr>
+              <div className="w-[10%] flex justify-center items-center">
+                <span className="pb-1">Or</span>
+              </div>
+              <hr className="w-[45%] bg-slate-700 h-[1px] border-none"></hr>
+            </div>
+            <div className="flex justify-center items-center gap-3">
+              <div className="w-[135px] h-[35px] flex justify-center items-center rounded-md bg-orange-700 shadow-lg hover:bg-orange-600 cursor-pointer overflow-hidden transition-all duration-300 ease-in-outs">
+                <span>
+                  <FaGoogle />
+                </span>
+              </div>
+              <div className="w-[135px] h-[35px] flex justify-center items-center rounded-md bg-blue-700 shadow-lg hover:bg-blue-600 cursor-pointer overflow-hidden transition-all duration-300 ease-in-outs">
+                <span>
+                  <FaFacebook />
+                </span>
+              </div>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
