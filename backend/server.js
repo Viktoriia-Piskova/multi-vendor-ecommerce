b9853816efff059787dd87ae25bb75fbba13423a
piskova <v.piskova@beetroot.se>
2024-10-11 19:19:20 +0300

    Backend base setup

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => res.send("My backend"));
+
+app.listen(port, () => console.log("Server is running"));
