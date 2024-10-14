5e1070e8fd35109bd0684fbf9e64d93d34bb4270
piskova <v.piskova@beetroot.se>
2024-10-14 18:52:06 +0300

    Backend: authController base setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 require("dotenv").config();
 
 const port = process.env.PORT;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cookieParser())
+
+app.use("/api", require("./routes/authRoutes"));
 
 app.get("/", (req, res) => res.send("My backend"));
 
