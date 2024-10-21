44add71e410369af0fefcd4c1e11a873e7471584
piskova <v.piskova@beetroot.se>
2024-10-21 19:05:33 +0300

    Dashboard: stats cards design

--- ecommerce-react/src/views/admin/AdminDashboard.jsx
+++ ecommerce-react/src/views/admin/AdminDashboard.jsx
@@ -1,9 +1,49 @@
 import React from "react";
+import { MdCurrencyExchange, MdProductionQuantityLimits } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
 
 const AdminDashboard = () => {
   return (
-    <div className="">
-      <h1>AdminDashboard</h1>
+    <div className="px-2 md:px-7 py-5">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7">
+        <div className="flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3">
+          <div className="flex flex-col items-start justify-start text-[#5c5a5a]">
+            <h2 className="text-3xl font-bold">$1232</h2>
+            <p className="text-md 1font-medium">Total sales</p>
+          </div>
+          <div className="w-[40px] h-[47px] rounded-full bg-[#fa0305] flex justify-center items-center text-xl">
+            <MdCurrencyExchange className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+        <div className="flex justify-between items-center p-5 bg-[#fde2ff] rounded-md gap-3">
+          <div className="flex flex-col items-start justify-start text-[#5c5a5a]">
+            <h2 className="text-3xl font-bold">50</h2>
+            <p className="text-md 1font-medium">Products</p>
+          </div>
+          <div className="w-[40px] h-[47px] rounded-full bg-[#760077] flex justify-center items-center text-xl">
+            <MdProductionQuantityLimits className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+        <div className="flex justify-between items-center p-5 bg-[#e9feea] rounded-md gap-3">
+          <div className="flex flex-col items-start justify-start text-[#5c5a5a]">
+            <h2 className="text-3xl font-bold">32</h2>
+            <p className="text-md 1font-medium">Sellers</p>
+          </div>
+          <div className="w-[40px] h-[47px] rounded-full bg-[#038000] flex justify-center items-center text-xl">
+            <FaUsers className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+        <div className="flex justify-between items-center p-5 bg-[#ecebff] rounded-md gap-3">
+          <div className="flex flex-col items-start justify-start text-[#5c5a5a]">
+            <h2 className="text-3xl font-bold">54</h2>
+            <p className="text-md 1font-medium">Orders</p>
+          </div>
+          <div className="w-[40px] h-[47px] rounded-full bg-[#0200f8] flex justify-center items-center text-xl">
+            <FaCartShopping className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
