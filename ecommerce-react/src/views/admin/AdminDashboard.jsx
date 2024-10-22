1c03552e75e4fe827eff78662084aa3a25586bdf
piskova <v.piskova@beetroot.se>
2024-10-22 12:36:09 +0300

    Dashboard design: charts

--- ecommerce-react/src/views/admin/AdminDashboard.jsx
+++ ecommerce-react/src/views/admin/AdminDashboard.jsx
@@ -1,9 +1,97 @@
 import React from "react";
+import Chart from "react-apexcharts";
 import { MdCurrencyExchange, MdProductionQuantityLimits } from "react-icons/md";
 import { FaUsers } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 
 const AdminDashboard = () => {
+  const statsState = {
+    series: [
+      {
+        name: "Orders",
+        data: [23, 34, 45, 56, 76, 34, 23, 76, 78, 46, 55, 30],
+      },
+      {
+        name: "Revenue",
+        data: [63, 104, 95, 120, 76, 34, 23, 76, 78, 103, 98, 100],
+      },
+      {
+        name: "Sellers",
+        data: [13, 14, 15, 12, 77, 24, 23, 16, 18, 56, 118, 48],
+      },
+    ],
+    options: {
+      color: ["#181ee8", "#181ee8"],
+      plotOptions: {
+        radius: 30,
+      },
+      chart: {
+        background: "transparent",
+        foreColor: "#d0d2d6",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      strock: {
+        show: true,
+        curve: ["smooth", "straight", "stepline"],
+        lineCap: "butt",
+        colors: "#f0f0f0",
+        width: 0.5,
+        dashArray: 0,
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+      },
+      legend: {
+        position: "top",
+      },
+      responsive: [
+        {
+          breakpoint: 565,
+          yaxis: {
+            categories: [
+              "Jan",
+              "Feb",
+              "Mar",
+              "Apr",
+              "May",
+              "Jun",
+              "Jul",
+              "Aug",
+              "Sep",
+              "Oct",
+              "Nov",
+              "Dec",
+            ],
+          },
+          options: {
+            plotOptions: {
+              bar: {
+                horizontal: true,
+              },
+            },
+            chart: {
+              height: "550px",
+            },
+          },
+        },
+      ],
+    },
+  };
   return (
     <div className="px-2 md:px-7 py-5">
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7">
@@ -41,6 +129,19 @@ const AdminDashboard = () => {
           </div>
           <div className="w-[40px] h-[47px] rounded-full bg-[#0200f8] flex justify-center items-center text-xl">
             <FaCartShopping className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full flex flex-wrap mt-7">
+        <div className="w-full lg:w-7/12 lg:pr-3">
+          <div className="w-full bg-[#6a5fdf] p-4 rounded-md">
+            <Chart
+              options={statsState.options}
+              series={statsState.series}
+              type="bar"
+              height={350}
+            />
           </div>
         </div>
       </div>
