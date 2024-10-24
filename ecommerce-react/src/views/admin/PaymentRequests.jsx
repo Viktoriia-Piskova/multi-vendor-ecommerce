7bf2375f84522a0456fa04615cf7bc9e22f4aa3f
piskova <v.piskova@beetroot.se>
2024-10-24 17:19:18 +0300

    Payment requests design

---
+++ ecommerce-react/src/views/admin/PaymentRequests.jsx
@@ -0,0 +1,68 @@
+import React, { forwardRef } from "react";
+import { FixedSizeList } from "react-window";
+
+function handleOnWheel({ deltaY }) {
+  console.log("handleOnWheel", deltaY);
+}
+
+const outerElementType = forwardRef((props, ref) => (
+  <div ref={ref} onWheel={handleOnWheel} {...props}></div>
+));
+
+const PaymentRequests = () => {
+  const dummyPayments = [1, 2, 3, 4, 5, 6, 7, 8];
+  const Row = ({ index, style }) => {
+    return (
+      <div style={style} className="flex text-sm text-white font-medium">
+        <div className="w-[25%] p-2 whitespace-nowrap">{index + 1}</div>
+        <div className="w-[25%] p-2 whitespace-nowrap">$1231</div>
+        <div className="w-[25%] p-2 whitespace-nowrap">
+          <span className="py-[1px] px-[5px] bg-slate-300 text-blue-500 rounded-md text-sm">
+            Pending
+          </span>
+        </div>
+        <div className="w-[25%] p-2 whitespace-nowrap">25 Oct 2024</div>
+        <div className="w-[25%] p-2 whitespace-nowrap">
+          <button className="bg-indigo-500 rounded-sm shadow-lg hover:bg-indigo-500/50 px-3 py-[2px] text-white text-sm">
+            Confirm
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="px-2 lg:px-7 pt-5">
+      <div className="w-full p-4 bg-est-violet-bright rounded-md">
+        <h2 className="text-est-light-grey text-xl font-medium pb-5">
+          Withdrawal Request
+        </h2>
+        <div className="w-full">
+          <div className="w-full overflow-x-auto">
+            <div className="flex bg-est-blue-light uppercase text-xs min-w-[340px] font-bold rounded-md">
+              <div className="w-[25%] p-2">No</div>
+              <div className="w-[25%] p-2">Amount</div>
+              <div className="w-[25%] p-2">Status</div>
+              <div className="w-[25%] p-2">Date</div>
+              <div className="w-[25%] p-2">Action</div>
+            </div>
+            {
+              <FixedSizeList
+                style={{ minWidth: "340px" }}
+                className="list"
+                height={350}
+                itemCount={100}
+                itemSize={35}
+                outerElementType={outerElementType}
+              >
+                {Row}
+              </FixedSizeList>
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentRequests;
--- ecommerce-react/tailwind.config.js
+++ ecommerce-react/tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'est-light-grey': '#d0d2d6',
-        'est-violet-bright': '#6a5fdf',
-        'est-violet-medium': '#b1addf',
-        'est-violet-light': '#cdcae9',
-        'est-violet-additional': '#8288ed',
-
-      }
+        "est-light-grey": "#d0d2d6",
+        "est-violet-bright": "#6a5fdf",
+        "est-violet-medium": "#b1addf",
+        "est-violet-light": "#cdcae9",
+        "est-violet-additional": "#8288ed",
+        "est-blue-light": "#8288ed",
+      },
     },
   },
   plugins: [],
-}
+};
